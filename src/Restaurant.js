ff82166fa0a4a2d01b884582074794e159236302
ivanevacic <ivanevacicweb@gmail.com>
2018-03-30 00:44:46 +0200

    Finished showing info of the user that voted

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -4,10 +4,14 @@ import './Restaurant.css';
 
 class Restaurant extends Component {
   render () {
-    const { name, handleSelect, handleDeselect } = this.props
+    const { name, votes, handleSelect, handleDeselect } = this.props
     return (
       <article className="Restaurant">
         <h3> { name } </h3>
+        <ul>
+          {/* if votes exists show them*/}
+            { votes && map(votes, (vote, key) => <li key={key}> { vote }</li>) }
+        </ul>
         <button onClick={ handleSelect }>
           Yea, I'd go there
         </button>
